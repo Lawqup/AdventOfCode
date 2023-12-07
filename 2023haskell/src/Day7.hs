86e32a6902c462d7c4d7491585ae47977f46d256
Lawrence Qupty <lawrencequp@gmail.com>
2023-12-07 10:04:51 -0800

    cleaned up day 7

--- 2023haskell/src/Day7.hs
+++ 2023haskell/src/Day7.hs
@@ -5,36 +5,14 @@ import Data.Function (on)
 
 import Data.List (group, sort, sortBy)
 
-newtype Hand = Hand [Int] deriving (Show, Eq)
+type Hand = [Int]
 
-instance Ord Hand where
-  (Hand h1) <= (Hand h2) = let t1 = toType h1
-                               t2 = toType h2
-                           in t1 < t2 || (t1 == t2) && h1 <= h2
-    where
-      toType :: [Int] -> Int
-      toType xs = case applyJ xs of
-                    [5] -> 7
-                    [1, 4] -> 6
-                    [2, 3] -> 5
-                    [1, 1, 3] -> 4
-                    [1, 2, 2] -> 3
-                    [1, 1, 1, 2] -> 2
-                    _ -> 1
-
-      applyJ xs = let xs' = filter (/= 1) xs
-                      counts = sort (map length (group (sort xs')))
-                      nJ = 5 - length xs'
-                  in if nJ == 5 then [5]
-                     else init counts ++ [last counts + nJ]
-
--- (Times, Distances)
 parsed :: Bool -> IO [(Hand, Int)]
 parsed isP2 = do
   inp <- lines <$> fetchInput 2023 7 "day_7.txt"
   return $ map (execParser ((,) <$> handP <*> (wsP *> intP))) inp
   where
-    handP = Hand . map toVal <$> spanP (/= ' ')
+    handP = map toVal <$> spanP (/= ' ')
     toVal x = case x of
                 'A' -> 14
                 'K' -> 13
@@ -57,9 +35,9 @@ tally comparitor hands = sum
     $ sortBy (comparitor `on` fst) hands
 
 comparitorBase :: ([Int] -> [Int]) -> Hand -> Hand -> Ordering
-comparitorBase apply (Hand h1) (Hand h2) = let t1 = toType h1
-                                               t2 = toType h2
-                                           in compare t1 t2 <> compare h1 h2
+comparitorBase apply h1 h2 = let t1 = toType h1
+                                 t2 = toType h2
+                             in compare t1 t2 <> compare h1 h2
   where
     toType :: [Int] -> Int
     toType xs = case apply xs of

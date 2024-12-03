b6c2723651d522abc6e0958302c6b94628d9deda
Lawrence Qupty <lawqup@amazon.com>
2024-12-02 18:52:35 -0800

    day 2 O(1) soln

--- rust2024/src/day2.rs
+++ rust2024/src/day2.rs
@@ -1,4 +1,4 @@
-fn reports() -> Vec<Vec<i32>> {
+pub fn reports() -> Vec<Vec<i32>> {
     let input =
         std::fs::read_to_string("/Users/lawqup/projects/AdventOfCode/rust2024/src/input/d2.txt")
             .unwrap();
@@ -9,9 +9,40 @@ fn reports() -> Vec<Vec<i32>> {
         .collect()
 }
 
-fn is_safe(x: i32, y: i32, inc: bool) -> bool {
+pub fn is_safe(x: i32, y: i32, inc: bool) -> bool {
     let diff = x - y;
     ((inc && diff > 0) || (!inc && diff < 0)) && diff.abs() >= 1 && diff.abs() <= 3
+}
+
+pub fn get_bad_index(report: &Vec<i32>, skip: Option<usize>) -> Option<usize> {
+    let first;
+    let second;
+
+    if skip.is_some_and(|j| j == 0) {
+        first = 1;
+        second = 2;
+    } else if skip.is_some_and(|j| j == 1) {
+        first = 0;
+        second = 2;
+    } else {
+        first = 0;
+        second = 1;
+    }
+
+    let inc = report[first] < report[second];
+    for i in second..report.len() {
+        if skip.is_some_and(|j| j == i) {
+            continue;
+        }
+
+        let prev = if skip.is_some_and(|j| j == i - 1) { i - 2 } else { i - 1 };
+
+        if !is_safe(report[i], report[prev], inc) {
+            return Some(i);
+        }
+    }
+
+    None
 }
 
 #[cfg(test)]
@@ -22,14 +53,10 @@ mod tests {
     fn d2p1() {
         let mut safe = 0;
 
-        'outer: for report in reports() {
-            let inc = report[0] < report[1];
-            for i in 1..report.len() {
-                if !is_safe(report[i], report[i-1], inc) {
-                    continue 'outer;
-                }
-            }
-            safe += 1;
+        for report in reports() {
+            if get_bad_index(&report, None).is_none() {
+                safe += 1;
+            };
         }
 
         println!("{safe}");
@@ -39,21 +66,26 @@ mod tests {
     fn d2p2() {
         let mut safe = 0;
 
-        'outer: for report in reports().into_iter() {
-            'remove: for remove in 0..report.len() {
-                let mut removed = report.clone();
-                removed.remove(remove);
-
-                let inc = removed[0] < removed[1];
-                for i in 1..removed.len() {
-                    if !is_safe(removed[i], removed[i-1], inc) {
-                        continue 'remove;
-                    }
-                }
-
+        for report in reports() {
+            let Some(bad) = get_bad_index(&report, None) else {
                 safe += 1;
-                continue 'outer;
-            }
+                continue;
+            };
+
+            if get_bad_index(&report, Some(bad)).is_none() {
+                safe += 1;
+                continue;
+            };
+
+            if get_bad_index(&report, Some(bad - 1)).is_none() {
+                safe += 1;
+                continue;
+            };
+
+            if get_bad_index(&report, Some(0)).is_none() {
+                safe += 1;
+                continue;
+            };
         }
 
         println!("{safe}");
